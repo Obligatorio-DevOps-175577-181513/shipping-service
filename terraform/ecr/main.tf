1866028875c787302ba3fbae03b94d2201eeb8f1
Damian <dariztimuno23@gmail.com>
2024-07-09 23:00:22 -0300

    primer intento de creacion con terraform de un contenedor

---
+++ terraform/ecr/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "shipping_service" {
+  name                 = "shipping-service"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "shipping-service"
+  }
+}
